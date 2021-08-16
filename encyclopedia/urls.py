a99af74224472e7de5fde73f61d2eca7d888803f
Bartlomiej Mikler <bartlomiej.mikler@gmail.com>
2021-08-16 20:09:57 +0200

    add edit page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<str:entry>", views.entry, name="entry"), 
     path("search/", views.search, name="search"),
-    path("random/", views.random_page, name="random_page")
+    path("random/", views.random_page, name="random_page"),
+    path("new_page/", views.new_page, name="new_page"),
+    path("edit_page/<str:entry>", views.edit_page, name="edit_page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,34 +5,49 @@ import random
 
 from . import util
 
+# form for search
 
-class SearchForm (forms.Form):
+
+class SearchForm(forms.Form):
     q = forms.CharField()
+
+# form for new page
+
+
+class NewPageForm(forms.Form):
+    title = forms.CharField()
+    content = forms.CharField()
 
 
 def index(request):
 
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries(),
-        "form": SearchForm()
+        "entries": util.list_entries()
     })
 
 
 def entry(request, entry):
 
+    if request.method == "POST":
+        return redirect('edit_page', entry=entry)
+
     # check if entry exist in entries directory
     try:
         text = markdown.markdown(util.get_entry(entry))
+        edit = True
     except:
         text = "<h1>Entry not found!</h1>"
+        edit = False
 
     # render page with entry text
     return render(request, "encyclopedia/entry.html", {
-        "entry": text
+        "entry": text,
+        "edit": edit
     })
 
 
 def search(request):
+
     if request.method == "POST":
 
         # fill the form by informations posted by form
@@ -42,10 +57,9 @@ def search(request):
             query = form.cleaned_data["q"]
 
             # check if query is in the list of entries
-            for entry in util.list_entries():
-                if query.lower() == entry.lower():
-                    # redirect to entry with entry name entry
-                    return redirect('entry', entry=entry)
+            if list_check(query, util.list_entries()):
+                # redirect to entry with entry name entry
+                return redirect('entry', entry=query)
 
             list = []
 
@@ -72,10 +86,54 @@ def search(request):
     else:
         return redirect('index')
 
+
+def new_page(request):
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+
+            title = form.cleaned_data['title']
+            content = "#" + title + "\n" + form.cleaned_data['content']
+
+            # check if the title is in the list of entries
+            if list_check(title, util.list_entries()):
+                return render(request, "encyclopedia/new_page.html", {
+                    "message": "Error. This entry exist in the wiki."
+                })
+
+            else:
+                util.save_entry(title, content)
+                return redirect('entry', entry=title)
+        else:
+            return render(request, "encyclopedia/new_page.html", {
+                "message": "Please fill all fields."
+            })
+
+    return render(request, "encyclopedia/new_page.html")
+
+
+def edit_page(request, entry):
+
+    title = entry
+    content = util.get_entry(title)
+
+    return render(request, "encyclopedia/edit_page.html", {
+        "title": title,
+        "content": content
+    })
+
+
 def random_page(request):
     entry_no = (random.randrange(len(util.list_entries())))
     entry = util.list_entries()[entry_no]
     return redirect('entry', entry=entry)
 
+# function to check if title is in the wiki
 
 
+def list_check(title, list):
+    for entry in list:
+        if title.lower() == entry.lower():
+            return True
+
+    return False
