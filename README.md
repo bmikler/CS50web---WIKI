cede3593e7a6aded6e972ee4a0d143b0dbb278f1
Bartlomiej Mikler <bartlomiej.mikler@gmail.com>
2021-10-01 19:09:20 +0200

    readme

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Wiki
+### Online encyclopedia, where users can search, add and edit pages.
+#### Application created during CS50 web programming course.
+
+# [Video presentation](https://www.youtube.com/watch?v=6TgSttoJ6jw)
+
--- encyclopedia/admin.py
+++ encyclopedia/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from auctions.models import Article, AuctionList, Category, Comment
+from django.contrib import admin
+
+from .models import *
+
+# Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Article)
+admin.site.register(AuctionList)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Watchlist)
